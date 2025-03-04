7ecedd8d8134a70d211783a2e8b7ec5264359bbc
lee <likachich12@gmail.com>
2025-03-04 13:30:05 +0300

    list hpp

--- hismatova.elizaveta/S1/list.hpp
+++ hismatova.elizaveta/S1/list.hpp
@@ -67,6 +67,80 @@ namespace hismatova
   {
     return Iterator< T >(fakeNode);
   }
+  template< typename T >
+  void List< T >::push_front(const T& data)
+  {
+    Node* newNode = new Node{data, fakeNode->next};
+    fakeNode->next = newNode;
+    size++;
+  }
+  template< typename T >
+  List< T >::List(const List< T >& num):
+    List()
+  {
+    auto temp = num.begin();
+    for (size_t i = num.size(); i > 0; i--)
+    {
+      push_front(*temp);
+      temp++;
+    }
+    reverse();
+  }
+  template< typename T >
+  void List< T >::clear()
+  {
+    Node< T >* temp = fakeNode->next;
+    while (size)
+    {
+      Node< T >* current = temp->next;
+      delete temp;
+      temp = current;
+      size--;
+    }
+  }
+  template< class T >
+  void List< T >::reverse()
+  {
+    if (fakeNode->next == fakeNode)
+    {
+      return;
+    }
+    Node< T >* next = fakeNode->next;
+    Node< T >* temp = fakeNode;
+    while (next->next != fakeNode)
+    {
+      Node< T >* temp2 = next->next;
+      next->next = temp;
+      temp = next;
+      next = temp2;
+    }
+    next->next = temp;
+    fakeNode->next = next;
+  }
+  template< typename T >
+  List< T >::List():
+    fakeNode(reinterpret_cast< NodeList< T >* >(new char[sizeof(NodeList< T >)])),
+    size(0)
+  {
+    fakeNode->next = fakeNode;
+  }
+  template< typename T >
+  void List< T >::swap(List< T >& num)
+  {
+    std::swap(fakeNode, num.fakeNode);
+    std::swap(size, num.size);
+  }
+  template < typename T >
+  void List< T >::pop_front()
+  {
+    fakeNode->next = fakeNode->next->next;
+    if (fakeNode->next == fakeNode)
+    {
+      return;
+    }
+    delete fakeNode->next;
+    size--;
+  }
 }
 
 #endif
