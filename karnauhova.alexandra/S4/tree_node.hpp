f1e266ae40cb5e6ebcffcaa9317be54eacbc8df6
“Karnaukhova <masj280906@gmail.com>
2025-04-15 02:03:37 +0300

    +node tree

---
+++ karnauhova.alexandra/S4/tree_node.hpp
@@ -0,0 +1,21 @@
+#ifndef TREE_NODE_HPP
+#define TREE_NODE_HPP
+
+namespace karnauhova
+{
+  enum Color 
+  {
+	Red,
+	Black
+  };
+
+  template< typename Key, typename Value >
+  struct TreeNode
+  {
+    Color color;
+    std::pair< Key, Value > data;
+    TreeNode< Key, Value >* left, * right, * parent;
+  };
+}
+
+#endif
