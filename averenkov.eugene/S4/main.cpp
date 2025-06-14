f636c0e67969bfae7a0402e6f4dfb3966490e364
Savishna <bravissimo28@mail.ru>
2025-06-14 22:02:35 +0300

    main

--- averenkov.eugene/S4/main.cpp
+++ averenkov.eugene/S4/main.cpp
@@ -1,146 +1,168 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <functional>
 #include <BiTree.hpp>
 
-using Dictionary = averenkov::Tree< int, std::string >;
-using DictionaryStorage = averenkov::Tree< std::string, Dictionary >;
-using DicFunc = std::function< void(DictionaryStorage&, const std::string&) >;
-using CommandTree = averenkov::Tree< std::string, DicFunc >;
-using str = const std::string&;
-
-void loadDictionaries(str filename, DictionaryStorage& storage)
+namespace averenkov
 {
-  std::ifstream file(filename);
-  if (!file)
-  {
-    std::cerr << "file error\n";
-    return;
-  }
+  using Dictionary = Tree<int, std::string>;
+  using DictionaryStorage = Tree<std::string, Dictionary>;
+  using DicFunc = std::function<void(DictionaryStorage&, const std::string&)>;
+  using CommandTree = Tree<std::string, DicFunc>;
 
-  std::string line;
-  while (std::getline(file, line))
+  void loadDictionaries(const std::string& filename, DictionaryStorage& storage)
   {
-    if (line.empty())
+    std::ifstream file(filename);
+    if (!file)
     {
-      continue;
+      std::cerr << "file error\n";
+      return;
     }
 
-    size_t space_pos = line.find(' ');
-    if (space_pos == std::string::npos)
+    std::string line;
+    while (std::getline(file, line))
     {
-      continue;
-    }
-
-    std::string dictName = line.substr(0, space_pos);
-    Dictionary dict;
-    size_t pos = space_pos + 1;
-    while (pos < line.length())
-    {
-      size_t next_space = line.find(' ', pos);
-      if (next_space == std::string::npos)
-      {
-        break;
-      }
-      std::string key_str = line.substr(pos, next_space - pos);
-      pos = next_space + 1;
-      next_space = line.find(' ', pos);
-      if (next_space == std::string::npos)
-      {
-        next_space = line.length();
-      }
-      std::string value = line.substr(pos, next_space - pos);
-      pos = next_space + 1;
-      try
-      {
-        int key = std::stoi(key_str);
-        dict.push(key, value);
-      }
-      catch (...)
+      if (line.empty())
       {
         continue;
       }
+
+      size_t space_pos = line.find(' ');
+      if (space_pos == std::string::npos)
+      {
+        continue;
+      }
+
+      std::string dictName = line.substr(0, space_pos);
+      Dictionary dict;
+      size_t pos = space_pos + 1;
+
+      while (pos < line.length())
+      {
+        size_t next_space = line.find(' ', pos);
+        if (next_space == std::string::npos)
+        {
+          break;
+        }
+
+        std::string key_str = line.substr(pos, next_space - pos);
+        pos = next_space + 1;
+        next_space = line.find(' ', pos);
+        if (next_space == std::string::npos)
+        {
+          next_space = line.length();
+        }
+
+        std::string value = line.substr(pos, next_space - pos);
+        pos = next_space + 1;
+
+        try
+        {
+          int key = std::stoi(key_str);
+          dict.push(key, value);
+        }
+        catch (...)
+        {
+          continue;
+        }
+      }
+
+      storage.push(dictName, dict);
+    }
+  }
+
+  void printDictionary(DictionaryStorage& storage, const std::string& dictName)
+  {
+    auto dict = storage.find(dictName);
+    if (dict == storage.end() || dict->second.empty())
+    {
+      std::cout << "<EMPTY>\n";
+      return;
     }
 
-    storage.push(dictName, dict);
-  }
-}
-
-void printDictionary(const Dictionary& dict, str name)
-{
-  if (dict.empty())
-  {
-    std::cout << "<EMPTY>\n";
-    return;
+    std::cout << dictName;
+    for (auto it = dict->second.begin(); it != dict->second.end(); ++it)
+    {
+      std::cout << " " << it->first << " " << it->second;
+    }
+    std::cout << "\n";
   }
 
-  std::cout << name;
-  for (auto it = dict.begin(); it != dict.end(); ++it)
+  void complement(DictionaryStorage& storage, const std::string& args)
   {
-    std::cout << " " << it->first << " " << it->second;
+    size_t pos = 0;
+    size_t space1 = args.find(' ', pos);
+    size_t space2 = args.find(' ', space1 + 1);
+    std::string newName = args.substr(pos, space1);
+    std::string name1 = args.substr(space1 + 1, space2 - space1 - 1);
+    std::string name2 = args.substr(space2 + 1);
+    auto dict1 = storage.find(name1);
+    auto dict2 = storage.find(name2);
+    Dictionary result;
+    for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
+    {
+      if (dict2->second.find(it->first) == dict2->second.end())
+      {
+        result.push(it->first, it->second);
+      }
+    }
+    storage.push(newName, result);
   }
 
-  std::cout << "\n";
-}
-
-void complement(DictionaryStorage& storage, str newName, str name1, str name2)
-{
-  auto dict1 = storage.find(name1);
-  auto dict2 = storage.find(name2);
-  Dictionary result;
-  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
+  void intersect(DictionaryStorage& storage, const std::string& args)
   {
-    if (dict2->second.find(it->first) == dict2->second.end())
+    size_t pos = 0;
+    size_t space1 = args.find(' ', pos);
+    size_t space2 = args.find(' ', space1 + 1);
+    std::string newName = args.substr(pos, space1);
+    std::string name1 = args.substr(space1 + 1, space2 - space1 - 1);
+    std::string name2 = args.substr(space2 + 1);
+    auto dict1 = storage.find(name1);
+    auto dict2 = storage.find(name2);
+    Dictionary result;
+    for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
+    {
+      if (dict2->second.find(it->first) != dict2->second.end())
+      {
+        result.push(it->first, it->second);
+      }
+    }
+    storage.push(newName, result);
+  }
+
+  void unionDicts(DictionaryStorage& storage, const std::string& args)
+  {
+    size_t pos = 0;
+    size_t space1 = args.find(' ', pos);
+    size_t space2 = args.find(' ', space1 + 1);
+    std::string newName = args.substr(pos, space1);
+    std::string name1 = args.substr(space1 + 1, space2 - space1 - 1);
+    std::string name2 = args.substr(space2 + 1);
+    auto dict1 = storage.find(name1);
+    auto dict2 = storage.find(name2);
+    Dictionary result;
+    for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
     {
       result.push(it->first, it->second);
     }
-  }
-
-  storage.push(newName, result);
-}
-
-void intersect(DictionaryStorage& storage, str newName, str name1, str name2)
-{
-  auto dict1 = storage.find(name1);
-  auto dict2 = storage.find(name2);
-  Dictionary result;
-  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
-  {
-    if (dict2->second.find(it->first) != dict2->second.end())
+    for (auto it = dict2->second.begin(); it != dict2->second.end(); ++it)
     {
-      result.push(it->first, it->second);
+      if (result.find(it->first) == result.end())
+      {
+        result.push(it->first, it->second);
+      }
     }
+    storage.push(newName, result);
   }
 
-  storage.push(newName, result);
-}
-
-void unionDicts(DictionaryStorage& storage, str newName, str name1, str name2)
-{
-  auto dict1 = storage.find(name1);
-  auto dict2 = storage.find(name2);
-  Dictionary result;
-  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
+  void commandsInit(CommandTree& commands)
   {
-    result.push(it->first, it->second);
+    commands["print"] = std::bind(printDictionary, std::placeholders::_1, std::placeholders::_2);
+    commands["complement"] = std::bind(complement, std::placeholders::_1, std::placeholders::_2);
+    commands["intersect"] = std::bind(intersect, std::placeholders::_1, std::placeholders::_2);
+    commands["union"] = std::bind(unionDicts, std::placeholders::_1, std::placeholders::_2);
   }
-  for (auto it = dict2->second.begin(); it != dict2->second.end(); ++it)
-  {
-    if (result.find(it->first) == result.end())
-    {
-      result.push(it->first, it->second);
-    }
-  }
-
-  storage.push(newName, result);
-}
-
-void commandsInit(CommandTree& commands)
-{
-  commands["print"] = std::bind(printDictionary, std::placeholders::_1, std::placeholders::_2);
-  commands["complement"] = std::bind(complement, std::placeholders::_1, std::placeholders::_2);
-  commands["intersect"] = std::bind(intersect, std::placeholders::_1, std::placeholders::_2);
-  commands["union"] = std::bind(unionDicts, std::placeholders::_1, std::placeholders::_2);
 }
 
 int main(int argc, char* argv[])
@@ -151,8 +173,11 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  DictionaryStorage dictionaries;
-  loadDictionaries(argv[1], dictionaries);
+  averenkov::DictionaryStorage dictionaries;
+  averenkov::loadDictionaries(argv[1], dictionaries);
+
+  averenkov::CommandTree commands;
+  averenkov::commandsInit(commands);
 
   std::string line;
   while (std::getline(std::cin, line))
@@ -164,58 +189,18 @@ int main(int argc, char* argv[])
 
     size_t space_pos = line.find(' ');
     std::string command = line.substr(0, space_pos);
-//    CommandTree commands;
-  //  commandsInit(commands);
-    //commands
-    if (command == "print")
+    std::string args = space_pos == std::string::npos ? "" : line.substr(space_pos + 1);
+
+    auto cmd = commands.find(command);
+    if (cmd != commands.end())
     {
-      if (space_pos == std::string::npos)
+      try
+      {
+        cmd->second(dictionaries, args);
+      }
+      catch (...)
       {
         std::cout << "<INVALID COMMAND>\n";
-        continue;
-      }
-      std::string dictName = line.substr(space_pos + 1);
-      auto dict = dictionaries.find(dictName);
-      if (dict != dictionaries.end())
-      {
-        printDictionary(dict->second, dictName);
-      }
-      else
-      {
-        std::cout << "<INVALID COMMAND>\n";
-      }
-    }
-    else if (command == "complement" || command == "intersect" || command == "union")
-    {
-      size_t pos1 = space_pos + 1;
-      size_t pos2 = line.find(' ', pos1);
-      size_t pos3 = line.find(' ', pos2 + 1);
-      if (pos1 == std::string::npos || pos2 == std::string::npos || pos3 == std::string::npos)
-      {
-        std::cout << "<INVALID COMMAND>\n";
-        continue;
-      }
-      std::string newName = line.substr(pos1, pos2 - pos1);
-      std::string name1 = line.substr(pos2 + 1, pos3 - pos2 - 1);
-      std::string name2 = line.substr(pos3 + 1);
-      auto dict1 = dictionaries.find(name1);
-      auto dict2 = dictionaries.find(name2);
-      if (dict1 == dictionaries.end() || dict2 == dictionaries.end())
-      {
-        std::cout << "<INVALID COMMAND>\n";
-        continue;
-      }
-      if (command == "complement")
-      {
-        complement(dictionaries, newName, name1, name2);
-      }
-      else if (command == "intersect")
-      {
-        intersect(dictionaries, newName, name1, name2);
-      }
-      else
-      {
-        unionDicts(dictionaries, newName, name1, name2);
       }
     }
     else
