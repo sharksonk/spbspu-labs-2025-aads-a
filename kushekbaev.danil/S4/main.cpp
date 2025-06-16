23aa9a1dbd8a6e1fdce5c3629b6dda3c8e8f2833
Z-1337 <un228der@gmail.com>
2025-06-16 21:33:07 +0300

    Changed return codes

--- kushekbaev.danil/S4/main.cpp
+++ kushekbaev.danil/S4/main.cpp
@@ -59,12 +59,12 @@ int main(int argc, char* argv[])
     catch (const std::out_of_range&)
     {
       std::cout << "<INVALID COMMAND>";
-      return 0;
+      return 1;
     }
     catch (const std::runtime_error&)
     {
       std::cout << "<EMPTY>";
-      return 1;
+      return 0;
     }
     catch (const std::exception& e)
     {
