b55c14b5aac43d593b39818c0e5b000b9ab0a33e
A1exCRE <alsmmm2065@gmail.com>
2025-06-20 14:35:10 +0300

    implementations of the constructor and basic methods

--- smirnov.alexandr/S7/hashTable.hpp
+++ smirnov.alexandr/S7/hashTable.hpp
@@ -17,6 +17,10 @@ namespace smirnov
     HashTable(HashTable && rhs) noexcept = default;
     HashTable & operator=(const HashTable & rhs) = default;
     HashTable & operator=(HashTable &&) noexcept = default;
+
+    size_t size() const noexcept;
+    bool empty() const noexcept;
+    float load_factor() const noexcept;
   private:
     std::vector< Bucket< Key, Value > > buckets_;
     size_t size_;
@@ -26,5 +30,53 @@ namespace smirnov
     size_t probe(size_t hash_value, size_t attempt) const noexcept;
     void rehash(size_t new_capacity);
   };
+
+  template< class Key, class Value, class Hash, class Equal >
+  size_t HashTable< Key, Value, Hash, Equal >::size() const noexcept
+  {
+    return size_;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  bool HashTable< Key, Value, Hash, Equal >::empty() const noexcept
+  {
+    return size_ == 0;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  float HashTable< Key, Value, Hash, Equal >::load_factor() const noexcept
+  {
+    if (size_ == 0)
+    {
+      return 0.0;
+    }
+    return static_cast< float >(size_) / buckets_.size();
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< Key, Value, Hash, Equal >::HashTable():
+    buckets_(8),
+    size_(0),
+    hasher_(),
+    key_equal_()
+  {}
+
+  template< class Key, class Value, class Hash, class Equal >
+  size_t HashTable< Key, Value, Hash, Equal >::probe(size_t hash_value, size_t attempt) const noexcept
+  {
+    return (hash_value + attempt * attempt) % buckets_.size();
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  void HashTable< Key, Value, Hash, Equal >::rehash(size_t new_capacity)
+  {
+    for (size_t i = 0; i < buckets_.size(); i++)
+    {
+      if (!buckets_[i].occupied)
+      {
+        continue;
+      }
+    }
+  }
 }
 #endif
--- smirnov.alexandr/S7/main.cpp
+++ smirnov.alexandr/S7/main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
 #include <string>
 #include "hashTable.hpp"
 
 int main()
 {
   smirnov::HashTable< int, std::string > table;
+  std::cout << "Size: " << table.size() << "\n";
+  std::cout << "Empty: " << (table.empty() ? "true" : "false") << "\n";
+  std::cout << "Load factor: " << table.load_factor() << "\n";
 }
