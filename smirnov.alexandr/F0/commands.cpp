dfd1d8aec6fae4289aa2709f8c043ebf87b87f28
A1exCRE <alsmmm2065@gmail.com>
2025-07-10 13:47:07 +0300

    create array

--- smirnov.alexandr/F0/commands.cpp
+++ smirnov.alexandr/F0/commands.cpp
@@ -1,4 +1,5 @@
 #include "commands.hpp"
+#include "array.hpp"
 #include <string>
 
 void smirnov::printCommand(Dicts & dicts, std::istream & in, std::ostream & out)
@@ -25,7 +26,7 @@ void smirnov::printCommand(Dicts & dicts, std::istream & in, std::ostream & out)
   for (auto it = dict.cbegin(); it != dict.cend(); ++it)
   {
     out << it->first << " - ";
-    const std::vector< std::string > & translations = it->second;
+    const Array & translations = it->second;
     for (std::size_t i = 0; i < translations.size(); ++i)
     {
       out << translations[i];
@@ -36,4 +37,46 @@ void smirnov::printCommand(Dicts & dicts, std::istream & in, std::ostream & out)
     }
     out << "\n";
   }
+}
+
+void smirnov::addCommand(Dicts & dicts, std::istream & in, std::ostream & out)
+{
+  std::string dictName, word, translation;
+  in >> dictName >> word >> translation;
+  if (!in)
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  auto dictIt = dicts.find(dictName);
+  if (dictIt == dicts.end())
+  {
+    out << "The dictionary with name " << dictName << " doesn't exist.\n";
+    return;
+  }
+
+  Dict & dict = dictIt->second;
+  auto it = dict.begin();
+  for (; it != dict.end(); ++it)
+  {
+    if (it->first == word)
+    {
+      Array & translations = it->second;
+      for (std::size_t i = 0; i < translations.size(); ++i)
+      {
+        if (translations[i] == translation)
+        {
+          out << "The word " << word << " already exists in " << dictName << "\n";
+          return;
+        }
+      }
+      translations.push_back(translation);
+      return;
+    }
+  }
+  Array arr;
+  arr.push_back(translation);
+  dict[word] = arr;
+  // По ТЗ — если слово добавлено впервые, ничего не выводим
 }
