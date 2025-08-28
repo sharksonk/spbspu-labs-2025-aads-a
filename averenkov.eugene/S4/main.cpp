6724c4f271fb5757fd365e8bd1e9809f1d597de1
Savishna <bravissimo28@mail.ru>
2025-08-28 20:16:47 +0300

    a

--- averenkov.eugene/S4/main.cpp
+++ averenkov.eugene/S4/main.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <stdexcept>
 #include <string>
@@ -92,7 +92,7 @@ namespace averenkov
     std::cout << "\n";
   }
 
-//  void complement(DictionaryStorage& storage, const std::string& args)
+  void complement(DictionaryStorage& storage, const std::string& args)
   {
     size_t pos = 0;
     size_t space1 = args.find(' ', pos);
@@ -119,32 +119,7 @@ namespace averenkov
       }
     }
     storage.push(newName, result);
-  }//
-void complement(DictionaryStorage& storage, const std::string& args)
-{
-  size_t space1 = args.find(' ');
-  size_t space2 = args.find(' ', space1 + 1);
-  if (space1 == std::string::npos || space2 == std::string::npos)
-  {
-    throw std::invalid_argument("<INVALID COMMAND>");
   }
-  std::string newName = args.substr(0, space1);
-  std::string name1 = args.substr(space1 + 1, space2 - space1 - 1);
-  std::string name2 = args.substr(space2 + 1);
-  auto dict1_iter = storage.find(name1);
-  auto dict2_iter = storage.find(name2);
-  Dictionary result;
-  Dictionary& dict1 = dict1_iter->second;
-  Dictionary& dict2 = dict2_iter->second;
-  for (auto it = dict1.begin(); it != dict1.end(); ++it)
-  {
-    if (dict2.find(it->first) == dict2.end())
-    {
-      result.push(it->first, it->second);
-    }
-  }
-  storage.push(newName, result);
-}
 
   void intersect(DictionaryStorage& storage, const std::string& args)
   {
@@ -252,226 +227,8 @@ int main(int argc, char* argv[])
     catch (const std::out_of_range& e)
     {
       std::cout << "<EMPTY>\n";
-      return 0;
     }
     catch (const std::invalid_argument& e)
-    {
-      std::cout << "<INVALID COMMAND>\n";
-      return 0;
-    }
-  }
-
-  return 0;
-}
-*/
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include "BiTree.hpp"
-
-using Dictionary = averenkov::Tree< int, std::string >;
-using DictionaryStorage = averenkov::Tree< std::string, Dictionary >;
-using str = const std::string&;
-
-void loadDictionaries(str filename, DictionaryStorage& storage)
-{
-  std::ifstream file(filename);
-  if (!file)
-  {
-    std::cerr << "file error\n";
-    return;
-  }
-
-  std::string line;
-  while (std::getline(file, line))
-  {
-    if (line.empty())
-    {
-      continue;
-    }
-
-    std::istringstream iss(line);
-    std::string dictName;
-    iss >> dictName;
-
-    Dictionary dict;
-    int key;
-    std::string value;
-    while (iss >> key >> value)
-    {
-      dict.push(key, value);
-    }
-
-    storage.push(dictName, dict);
-  }
-}
-
-void printDictionary(const Dictionary& dict, str name)
-{
-  if (dict.empty())
-  {
-    std::cout << "<EMPTY>\n";
-    return;
-  }
-
-  std::cout << name;
-  for (auto it = dict.begin(); it != dict.end(); ++it)
-  {
-    std::cout << " " << it->first << " " << it->second;
-  }
-
-  std::cout << "\n";
-}
-
-void complement(DictionaryStorage& storage, str newName, str name1, str name2)
-{
-  auto dict1 = storage.find(name1);
-  auto dict2 = storage.find(name2);
-
-  if (dict1 == storage.end() || dict2 == storage.end())
-  {
-    std::cout << "<INVALID COMMAND>\n";
-    return;
-  }
-
-  Dictionary result;
-  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
-  {
-    if (dict2->second.find(it->first) == dict2->second.end())
-    {
-      result.push(it->first, it->second);
-    }
-  }
-
-  storage.push(newName, result);
-}
-
-void intersect(DictionaryStorage& storage, str newName, str name1, str name2)
-{
-  auto dict1 = storage.find(name1);
-  auto dict2 = storage.find(name2);
-
-  if (dict1 == storage.end() || dict2 == storage.end())
-  {
-    std::cout << "<INVALID COMMAND>\n";
-    return;
-  }
-
-  Dictionary result;
-  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
-  {
-    if (dict2->second.find(it->first) != dict2->second.end())
-    {
-      result.push(it->first, it->second);
-    }
-  }
-
-  storage.push(newName, result);
-}
-
-void unionDicts(DictionaryStorage& storage, str newName, str name1, str name2)
-{
-  auto dict1 = storage.find(name1);
-  auto dict2 = storage.find(name2);
-
-  if (dict1 == storage.end() || dict2 == storage.end())
-  {
-    std::cout << "<INVALID COMMAND>\n";
-    return;
-  }
-
-  Dictionary result;
-  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
-  {
-    result.push(it->first, it->second);
-  }
-  for (auto it = dict2->second.begin(); it != dict2->second.end(); ++it)
-  {
-    if (result.find(it->first) == result.end())
-    {
-      result.push(it->first, it->second);
-    }
-  }
-
-  storage.push(newName, result);
-}
-
-int main(int argc, char* argv[])
-{
-  if (argc != 2)
-  {
-    std::cerr << "Usage: " << argv[0] << " filename\n";
-    return 1;
-  }
-
-  DictionaryStorage dictionaries;
-  loadDictionaries(argv[1], dictionaries);
-
-  std::string line;
-  while (std::getline(std::cin, line))
-  {
-    std::istringstream iss(line);
-    std::string command;
-    iss >> command;
-
-    if (command == "print")
-    {
-      std::string dictName;
-      if (iss >> dictName)
-      {
-        auto dict = dictionaries.find(dictName);
-        if (dict != dictionaries.end())
-        {
-          printDictionary(dict->second, dictName);
-        }
-        else
-        {
-          std::cout << "<INVALID COMMAND>\n";
-        }
-      }
-      else
-      {
-        std::cout << "<INVALID COMMAND>\n";
-      }
-    }
-    else if (command == "complement")
-    {
-      std::string newName, name1, name2;
-      if (iss >> newName >> name1 >> name2)
-      {
-        complement(dictionaries, newName, name1, name2);
-      }
-      else
-      {
-        std::cout << "<INVALID COMMAND>\n";
-      }
-    }
-    else if (command == "intersect")
-    {
-      std::string newName, name1, name2;
-      if (iss >> newName >> name1 >> name2)
-      {
-        intersect(dictionaries, newName, name1, name2);
-      }
-      else
-      {
-        std::cout << "<INVALID COMMAND>\n";
-      }
-    }
-    else if (command == "union")
-    {
-      std::string newName, name1, name2;
-      if (iss >> newName >> name1 >> name2)
-      {
-        unionDicts(dictionaries, newName, name1, name2);
-      }
-      else
-      {
-        std::cout << "<INVALID COMMAND>\n";
-      }
-    }
-    else
     {
       std::cout << "<INVALID COMMAND>\n";
     }
