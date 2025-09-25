a32abfcb14010792b45a177c5dc1a793e835801a
danil@debian1337 <un228der@gmail.com>
2025-09-25 23:09:30 +0300

    more cg changes

--- kushekbaev.danil/S2/ioutils.cpp
+++ kushekbaev.danil/S2/ioutils.cpp
@@ -1,7 +1,7 @@
 #include "ioutils.hpp"
 #include <iostream>
 
-void kushekbaev::splitExpr(const std::string& string, kushekbaev::Queue< std::string >& arithmeticQ)
+void kushekbaev::splitExpr(const std::string& string, kushekbaev::Queue< std::string >& arithmeticQueue)
 {
   std::string symbol;
   char delimiter = ' ';
@@ -11,24 +11,24 @@ void kushekbaev::splitExpr(const std::string& string, kushekbaev::Queue< std::st
   {
     end = string.find(delimiter, begin);
     symbol = string.substr(begin, end - begin);
-    arithmeticQ.push(symbol);
+    arithmeticQueue.push(symbol);
     begin = string.find_first_not_of(delimiter, end);
   }
 }
 
-void kushekbaev::inputExpr(std::istream& input, kushekbaev::Queue< kushekbaev::Queue< std::string > >& Q)
+void kushekbaev::inputExpr(std::istream& input, kushekbaev::Queue< kushekbaev::Queue< std::string > >& queue)
 {
   std::string string;
-  kushekbaev::Queue< std::string > infixQ;
+  kushekbaev::Queue< std::string > infixQueue;
   while (std::getline(input, string))
   {
     if (string.empty())
     {
       continue;
     }
-    infixQ.clear();
-    splitExpr(string, infixQ);
-    Q.push(std::move(infixQ));
+    infixQueue.clear();
+    splitExpr(string, infixQueue);
+    queue.push(std::move(infixQueue));
   }
 }
 
