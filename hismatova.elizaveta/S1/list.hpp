51b0e760a2dedce9d0ce7ba0af973ca3ae369ca4
lee <likachich12@gmail.com>
2025-10-03 14:42:41 +0300

    s1 fixes

--- hismatova.elizaveta/S1/list.hpp
+++ hismatova.elizaveta/S1/list.hpp
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <numeric>
 #include <utility>
+
 namespace hismatova
 {
   template < typename T >
@@ -13,10 +14,14 @@ namespace hismatova
     {
       T data;
       Node* next;
-      Node(const T& val, Node* nextNode = nullptr) : data(val), next(nextNode) {}
+      Node(const T& val, Node* nextNode = nullptr):
+        data(val),
+        next(nextNode)
+      {}
     };
-      Node* fake;
-      size_t list_size;
+    Node* fake;
+    size_t list_size;
+
   public:
     class iterator
     {
@@ -27,46 +32,78 @@ namespace hismatova
       using reference = T&;
       using iterator_category = std::forward_iterator_tag;
       Node* current;
-      explicit iterator(Node* node) : current(node) {}
-      T& operator*() { return current->data; }
+      explicit iterator(Node* node):
+        current(node)
+      {}
+      T& operator*()
+      {
+        return current->data;
+      }
+      T* operator->()
+      {
+        return &current->data;
+      }
       iterator& operator++()
       {
         current = current->next;
         return *this;
       }
-      bool operator!=(const iterator& other) const { return current != other.current; }
+      bool operator!=(const iterator& other) const
+      {
+        return current != other.current;
+      }
     };
-    List() : list_size(0)
+
+    List(): list_size(0)
     {
       fake = new Node(T());
       fake->next = fake;
     }
-    List(std::initializer_list< T > ilist) : List()
+
+    List(std::initializer_list< T > ilist): List()
     {
-      for (const auto& val : ilist) push_back(val);
+      for (const auto& val: ilist)
+      {
+        push_back(val);
+      }
     }
-    List(const List& other) : List()
+
+    List(const List& other): List()
     {
-      for (auto it = other.begin(); it != other.end(); ++it) push_back(*it);
+      for (auto it = other.begin(); it != other.end(); ++it)
+      {
+        push_back(*it);
+      }
     }
-    List(List&& other) noexcept : fake(other.fake), list_size(other.list_size)
+
+    List(List&& other) noexcept: fake(other.fake), list_size(other.list_size)
     {
       other.fake = nullptr;
       other.list_size = 0;
     }
-    ~List() { clear(); delete fake; }
+
+    ~List()
+    {
+      clear();
+      delete fake;
+    }
+
     List& operator=(const List& other)
     {
-      if (this != &other)
+      if (std::addressof(*this) != std::addressof(other))
       {
         clear();
-        for (auto it = other.begin(); it != other.end(); ++it) push_back(*it);
-       }
+        for (auto it = other.begin(); it != other.end(); ++it)
+        {
+          push_back(*it);
+        }
+      }
       return *this;
     }
+
     List& operator=(List&& other) noexcept
     {
-      if (this != &other)
+      if (std::addressof(*this) != std::addressof(other))
       {
         clear();
         delete fake;
@@ -77,78 +114,157 @@ namespace hismatova
       }
       return *this;
     }
+
     bool operator==(const List& other) const
     {
-      if (list_size != other.list_size) return false;
+      if (list_size != other.list_size)
+      {
+        return false;
+      }
       auto it1 = begin(), it2 = other.begin();
       while (it1 != end())
       {
-        if (*it1 != *it2) return false;
+        if (*it1 != *it2)
+        {
+          return false;
+        }
         ++it1; ++it2;
-       }
-       return true;
+      }
+      return true;
     }
-    bool operator!=(const List& other) const { return !(*this == other); }
+
+    bool operator!=(const List& other) const
+    {
+      return !(*this == other);
+    }
+
     bool operator<(const List& other) const
     {
       auto it1 = begin(), it2 = other.begin();
       while (it1 != end() && it2 != other.end())
       {
-        if (*it1 < *it2) return true;
-        if (*it2 < *it1) return false;
+        if (*it1 < *it2)
+        {
+          return true;
+        }
+        if (*it2 < *it1)
+        {
+          return false;
+        }
         ++it1; ++it2;
       }
       return list_size < other.list_size;
     }
-    bool operator>(const List& other) const { return other < *this; }
-    bool operator<=(const List& other) const { return !(*this > other); }
-    bool operator>=(const List& other) const { return !(*this < other); }
-    iterator begin() const { return iterator(fake->next); }
-    iterator end() const { return iterator(fake); }
-    T& front() { return fake->next->data; }
-    const T& front() const { return fake->next->data; }
+
+    bool operator>(const List& other) const
+    {
+      return other < *this;
+    }
+
+    bool operator<=(const List& other) const
+    {
+      return !(*this > other);
+    }
+
+    bool operator>=(const List& other) const
+    {
+      return !(*this < other);
+    }
+
+    iterator begin() const
+    {
+      return iterator(fake->next);
+    }
+
+    iterator end() const
+    {
+      return iterator(fake);
+    }
+
+    T& front()
+    {
+      return fake->next->data;
+    }
+
+    const T& front() const
+    {
+      return fake->next->data;
+    }
+
     T& back()
     {
       Node* last = fake;
-      while (last->next != fake) last = last->next;
+      while (last->next != fake)
+      {
+        last = last->next;
+      }
       return last->data;
     }
+
     const T& back() const
     {
       Node* last = fake;
-      while (last->next != fake) last = last->next;
+      while (last->next != fake)
+      {
+        last = last->next;
+      }
       return last->data;
     }
-    bool empty() const { return list_size == 0; }
-    size_t size() const { return list_size; }
+
+    bool empty() const
+    {
+      return list_size == 0;
+    }
+
+    size_t size() const
+    {
+      return list_size;
+    }
+
     void push_back(const T& value)
     {
       Node* newNode = new Node(value, fake);
       Node* last = fake;
-      while (last->next != fake) last = last->next;
+      while (last->next != fake)
+      {
+        last = last->next;
+      }
       last->next = newNode;
       ++list_size;
     }
+
     void pop_front()
     {
-      if (empty()) return;
+      if (empty())
+      {
+        return;
+      }
       Node* temp = fake->next;
       fake->next = temp->next;
       delete temp;
       --list_size;
     }
+
     void clear()
     {
-      while (!empty()) pop_front();
+      while (!empty())
+      {
+        pop_front();
+      }
     }
+
     void swap(List& other)
     {
       std::swap(fake, other.fake);
       std::swap(list_size, other.list_size);
     }
+
     void reverse()
     {
-      if (list_size < 2) return;
+      if (list_size < 2)
+      {
+        return;
+      }
       Node* prev = fake;
       Node* curr = fake->next;
       Node* next;
@@ -161,6 +277,7 @@ namespace hismatova
       }
       fake->next = prev;
     }
+
     void remove(const T& value)
     {
       Node* prev = fake;
@@ -181,6 +298,7 @@ namespace hismatova
         }
       }
     }
+
     void remove_if(bool (*predicate)(const T&))
     {
       Node* prev = fake;
@@ -201,39 +319,62 @@ namespace hismatova
         }
       }
     }
+
     void splice(iterator pos, List& other)
     {
-      if (other.empty()) return;
+      if (other.empty())
+      {
+        return;
+      }
       Node* last = fake;
-      while (last->next != fake) last = last->next;
+      while (last->next != fake)
+      {
+        last = last->next;
+      }
       last->next = other.fake->next;
       other.fake->next = other.fake;
       list_size += other.list_size;
       other.list_size = 0;
     }
+
     void assign(size_t count, const T& value)
     {
       clear();
-      for (size_t i = 0; i < count; ++i) push_back(value);
+      for (size_t i = 0; i < count; ++i)
+      {
+        push_back(value);
+      }
     }
-    void assign(std::initializer_list<T> ilist)
+
+    void assign(std::initializer_list< T > ilist)
     {
       clear();
-      for (const auto& val : ilist) push_back(val);
+      for (const auto& val: ilist)
+      {
+        push_back(val);
+      }
     }
+
     void insert(iterator pos, const T& value)
     {
       Node* newNode = new Node(value);
       Node* current = fake;
-      while (current != pos.current) current = current->next;
+      while (current != pos.current)
+      {
+        current = current->next;
+      }
       newNode->next = current->next;
       current->next = newNode;
       ++list_size;
     }
+
     void erase(iterator pos)
     {
       Node* current = fake;
-      while (current->next != pos.current) current = current->next;
+      while (current->next != pos.current)
+      {
+        current = current->next;
+      }
       Node* temp = current->next;
       current->next = temp->next;
       delete temp;
--- hismatova.elizaveta/S1/main.cpp
+++ hismatova.elizaveta/S1/main.cpp
@@ -1,86 +1,82 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <numeric>
-#include <sstream>
 #include <limits>
 #include "list.hpp"
 
 int main()
 {
   using namespace hismatova;
-  std::vector< std::pair< std::string, List< unsigned long long > > > sequences;
+  List< std::pair< std::string, List< unsigned long long > > > sequences;
   std::string name;
   while (std::cin >> name)
   {
     List< unsigned long long > numbers;
-    std::string line;
-    std::getline(std::cin, line);
-    if (line.empty())
-    {
-      line = "";
-    }
-    std::istringstream iss(line);
-    unsigned long long num = 0;
-    while (iss >> num)
+    unsigned long long num;
+    while (std::cin >> num)
     {
       numbers.push_back(num);
     }
-    sequences.emplace_back(name, numbers);
+    if (std::cin.fail() && !std::cin.eof())
+    {
+      std::cin.clear();
+    }
+    sequences.push_back({name, numbers});
   }
   if (sequences.empty())
   {
     std::cout << "0\n";
     return 0;
   }
-  for (size_t i = 0; i < sequences.size(); ++i)
+  const char* sep = "";
+  for (const auto& [first, second]: sequences)
   {
-    std::cout << sequences[i].first;
-    if (i + 1 < sequences.size())
-    {
-      std::cout << " ";
-    }
+    std::cout << sep << first;
+    sep = " ";
   }
   std::cout << "\n";
   size_t max_length = 0;
-  for (const auto& seq : sequences)
+  for (const auto& seq: sequences)
   {
     max_length = std::max(max_length, seq.second.size());
   }
-  std::vector< std::vector< unsigned long long > > vertical_sequences(max_length);
+  List< List< unsigned long long > > vertical_sequences;
   for (size_t i = 0; i < max_length; ++i)
   {
-    for (const auto& seq : sequences)
+    List< unsigned long long > inner_list;
+    for (const auto& seq: sequences)
     {
       if (i < seq.second.size())
       {
         auto it = seq.second.begin();
-        for (size_t j = 0; j < i; ++j) ++it;
-        vertical_sequences[i].push_back(*it);
+        for (size_t j = 0; j < i; ++j)
+        {
+          ++it;
+        }
+        inner_list.push_back(*it);
       }
     }
+    vertical_sequences.push_back(inner_list);
   }
-  for (const auto& v_seq : vertical_sequences)
+  for (const auto& v_seq: vertical_sequences)
   {
-    for (size_t j = 0; j < v_seq.size(); ++j)
+    const char* sep = "";
+    for (const auto& item: v_seq)
     {
-      std::cout << v_seq[j];
-      if (j + 1 < v_seq.size())
-      {
-        std::cout << " ";
-      }
+      std::cout << sep << item;
+      sep = " ";
     }
     std::cout << "\n";
   }
-  std::vector< unsigned long long > sums;
+  List< unsigned long long > sums;
   bool overflow_occurred = false;
-  for (const auto& v_seq : vertical_sequences)
+  for (const auto& v_seq: vertical_sequences)
   {
     unsigned long long sum = 0;
-    for (size_t i = 0; i < v_seq.size(); ++i)
+    for (const auto& item: v_seq)
     {
       unsigned long long prev_sum = sum;
-      sum += v_seq[i];
+      sum += item;
       if (sum < prev_sum)
       {
         overflow_occurred = true;
@@ -99,15 +95,12 @@ int main()
     std::cout << "0\n";
     return 0;
   }
-  for (size_t i = 0; i < sums.size(); ++i)
+  sep = "";
+  for (const auto& item: sums)
   {
-    std::cout << sums[i];
-    if (i +1 < sums.size())
-    {
-      std::cout << " ";
-    }
+    std::cout << sep << item;
+    sep = " ";
   }
   std::cout << "\n";
   return 0;
 }
-
