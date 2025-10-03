dbd97bb13810e99e3e47dd47bf3d561304b6dcc6
Maksim_Shramko <maxramko@gmail.com>
2025-10-03 22:24:24 +0300

    using tree

--- shramko.maksim/S4/commandProcessor.cpp
+++ shramko.maksim/S4/commandProcessor.cpp
@@ -1,25 +1,29 @@
 #include "commandProcessor.hpp"
 #include "commands.hpp"
+#include <functional>
 
 namespace shramko
 {
+  void printWrapper(TreeOfTrees& trees, std::istream& in, std::ostream& out)
+  {
+    print(trees, in, out);
+  }
+
   void processCommand(TreeOfTrees & trees, const std::string & cmd, std::istream & in, std::ostream & out)
   {
-    if (cmd == "print")
+    using CommandFunction = std::function< void(TreeOfTrees&, std::istream&, std::ostream&) >;
+    static UBstTree< std::string, CommandFunction > commandMap;
+    if (commandMap.empty())
     {
-      print(trees, in, out);
+      commandMap["print"] = printWrapper;
+      commandMap["complement"] = complement;
+      commandMap["intersect"] = intersect;
+      commandMap["union"] = unite;
     }
-    else if (cmd == "complement")
+    auto it = commandMap.find(cmd);
+    if (it != commandMap.cend())
     {
-      complement(trees, in, out);
-    }
-    else if (cmd == "intersect")
-    {
-      intersect(trees, in, out);
-    }
-    else if (cmd == "union")
-    {
-      unite(trees, in, out);
+      it->second(trees, in, out);
     }
     else
     {
