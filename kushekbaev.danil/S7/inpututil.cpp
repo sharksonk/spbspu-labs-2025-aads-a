a3f1a53abb1ea9bd9c407040534659c5c6728700
danil@debian1337 <un228der@gmail.com>
2025-10-13 19:46:17 +0300

    fix

--- kushekbaev.danil/S7/inpututil.cpp
+++ kushekbaev.danil/S7/inpututil.cpp
@@ -6,7 +6,7 @@ void kushekbaev::inputGraphs(std::istream& in, std::map< std::string, kushekbaev
   size_t count = 0;
   while (in >> name >> count)
   {
-    kushekbaev::Graph gr;
+    Graph gr;
     for (size_t i = 0; i < count; ++i)
     {
       std::string vect1, vect2;
